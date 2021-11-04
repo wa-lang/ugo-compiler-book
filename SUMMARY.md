e3f274942ac2ef65207d3a3ba9739a46ff9ea0ec
chai2010 <chaishushan@gmail.com>
2021-11-05 00:53:00 +0800

    ch2 done

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,3 +1,4 @@
 # Summary
 
 - [最小编译器](./ch1/readme.md)
+- [可加减的表达式语言](./ch2/readme.md)
---
+++ ch2/code-02/_output_amd64.s
@@ -0,0 +1,8 @@
+.intel_syntax noprefix
+.globl _main
+
+_main:
+    mov rax, 1
+    add rax, 2
+    add rax, 3
+    ret
