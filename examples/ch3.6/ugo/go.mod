cc233c236b2b05d2fb7d1f5c23e01874c783cbba
chai2010 <chaishushan@gmail.com>
2021-12-08 23:59:30 +0800

    zz

--- examples/ch3.6/ugo/go.mod
+++ examples/ch3.6/ugo/go.mod
@@ -1,3 +1,11 @@
 module github.com/chai2010/ugo
 
 go 1.17
+
+require github.com/urfave/cli/v2 v2.3.0
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+)
--- examples/ch3.6/ugo/main.go
+++ examples/ch3.6/ugo/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
+
+	"github.com/urfave/cli/v2"
 
 	"github.com/chai2010/ugo/ast"
 	"github.com/chai2010/ugo/compiler"
@@ -9,6 +12,58 @@ import (
 )
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "ugo"
+	app.Usage = "ugo is a tool for managing µGo source code."
+	app.Version = "0.0.1"
+
+	app.Commands = []*cli.Command{
+		{
+			Name:  "build",
+			Usage: "compile µGo source code",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+		{
+			Name:  "run",
+			Usage: "compile and run µGo program",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+		{
+			Name:  "ast",
+			Usage: "parse µGo source code and print ast",
+			Action: func(c *cli.Context) error {
+				if c.NArg() == 0 {
+					fmt.Fprintf(os.Stderr, "")
+					os.Exit(1)
+				}
+
+				code := loadCode("./hello.ugo")
+				f, err := parser.ParseFile("./hello.ugo", code)
+				if err != nil {
+					panic(err)
+				}
+
+				ast.Print(f)
+				return nil
+			},
+		},
+		{
+			Name:  "lex",
+			Usage: "lex µGo source code and print token list",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
+
+func main2() {
 	code := loadCode("./hello.ugo")
 	f, err := parser.ParseFile("./hello.ugo", code)
 	if err != nil {
