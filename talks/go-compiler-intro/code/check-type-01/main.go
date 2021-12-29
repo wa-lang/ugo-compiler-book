543e4ceda02f1fd782eb36942a62709a5fbd9623
chai2010 <chaishushan@gmail.com>
2021-12-30 01:33:14 +0800

    zz

---
+++ talks/go-compiler-intro/code/check-type-01/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"log"
+)
+
+func init() {
+	log.SetFlags(0)
+}
+
+func main() {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "hello.go", src, parser.AllErrors)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pkg, err := new(types.Config).Check("hello.go", fset, []*ast.File{f}, nil) // HL
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = pkg
+}
+
+const src = `package main
+func main() {
+	var _ = "a" + 1 // HL
+}
+`
